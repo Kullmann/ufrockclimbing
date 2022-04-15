2a6433ddebd2d64949a7c6f9c4fd61460c2e789c
Sean <seankullmann@gmail.com>
2022-04-15 03:03:21 -0400

    Add responsiveness to header and banner image

--- src/components/BodyContent.tsx
+++ src/components/BodyContent.tsx
@@ -10,8 +10,8 @@ function BodyContent() {
           radius="md"
           src="https://scontent-atl3-1.xx.fbcdn.net/v/t39.30808-6/215435897_1630717847123605_1045119515317379046_n.png?stp=dst-jpg_p480x480&_nc_cat=101&ccb=1-5&_nc_sid=8631f5&_nc_ohc=aXqN5xf1bTMAX-ttpPW&tn=T3utIPM-iYI7Ytlv&_nc_ht=scontent-atl3-1.xx&oh=00_AT-MbtpZ2RqDNtlZOfana_QqsldXr775lxEmk_omMeKKRg&oe=625D8015"
           alt="Florida Rock Climbing Club Banner"
-          height={480}
-          width={960}
+          height={"100%"}
+          width={"100%"}
         />
       <Grid><Grid.Col>
       <Title>About</Title>
--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -1,4 +1,4 @@
-import { Text, Button, Group, Title } from '@mantine/core';
+import { Text, Button, Group, Title, MediaQuery } from '@mantine/core';
 
 function HeaderContent() {
   return (<>
@@ -6,14 +6,16 @@ function HeaderContent() {
     <Group position='left' spacing="xs">
       <Title style={{ color: '#0021A5' }}>UF</Title><Title style={{ color: '#FA4616' }}>Rock Climbing</Title>
     </Group>
+    
+    <MediaQuery smallerThan="xs" styles={{ display: "none"}}>
+      <Group position = 'right'>
+        <Text variant="link" component="a" href="#">About</Text>
 
-    <Group position = 'right'>
-      <Text variant="link" component="a" href="#">About</Text>
+        <Text variant="link" component="a" href="#">Officers</Text>
 
-      <Text variant="link" component="a" href="#">Officers</Text>
-
-      <Button>Contact Us</Button>
-    </Group>
+        <Button>Contact Us</Button>
+      </Group>
+    </MediaQuery>
 
   </>);
 }
